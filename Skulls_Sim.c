f4f9f34fbb325b575586312766ab37cce4c8f8e3
jonyski <geleiaeavela@gmail.com>
2024-03-23 15:30:57 -0300

    adds rand() seed setting

--- Skulls_Sim.c
+++ Skulls_Sim.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "bots.h" // header containing the Bot struct, its dependencies and some handy functions
 #include "simgame.h" // header containing the necessary tools to simulate 1 game of Skull
 
 /* the first argument should be the number of games simulated
    and the second should be the number of bots playing (3 to 6) */
 int main(int argc, char const *argv[]) {
-
 	int numOfGames = atoi(argv[1]);
 	int numOfBots = atoi(argv[2]);
 	struct Bot* bots; // pointer to array objets (bots)
@@ -18,8 +18,9 @@ int main(int argc, char const *argv[]) {
 		bots = initializeBots(numOfBots);
 	}
 
+	srand(time(NULL));
 	for (int i = 0; i < numOfGames; i++){
-		//simulateGame();
+		simulateGame();
 	}
 
 	return 0;
